69cb598f3d3a51195a7dbf7c5723bc4f9716c89b
YucelT94 <yucelterlemezuglu@gmail.com>
2017-08-03 15:32:06 +0300

    more fix

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
+
 exports.index = function (req, res) {
-    res.json({date: new date()});
+    res.json({date: new Date()});
 };
